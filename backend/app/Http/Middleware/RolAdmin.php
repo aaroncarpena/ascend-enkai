da73901d3d44099dcec62f7841e05f8b24429fb8
aaroncarpena <aaroncarpenaperez@gmail.com>
2026-03-02 19:57:38 +0100

    Modificación de las peticiones HTTP para usar un enviroment, también añadido el middleware (Se me había olvidao)

---
+++ backend/app/Http/Middleware/RolAdmin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RolAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(auth()->check() && auth()->user()->rol !== 'admin'){
+            abort(403, 'No tienes permisos para acceder a está página');
+        }
+        return $next($request);
+    }
+}
